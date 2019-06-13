c2451de87e5c4b0c9fc7484735518e7c04b4c923
M4909588 <mj1505())>
2019-06-13 11:55:45 +0530

    Category Screen UI and Webapi call

--- lib/views/tabs/CategoriesTab.dart
+++ lib/views/tabs/CategoriesTab.dart
@@ -1,30 +1,187 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meetup_login/InviteList.dart';
+import 'package:flutter_meetup_login/viewmodel/Categories.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class CategoriesTab extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.white,
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            // center the children
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Icon(
-                Icons.category,
-                size: 160.0,
-                color: Colors.blue,
-              ),
-              new Text(
-                "First Tab",
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
 
-                style: new TextStyle(color: Colors.black),
-              )
-            ],
+class CategoriesTabs extends StatefulWidget {
+  TabController _tabController;
+  CategoriesTabs(this._tabController, {Key key, this.title}) : super(key: key);
+
+  final String title;
+
+
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState(controller: this._tabController);
+}
+
+class _MyHomePageState extends State<CategoriesTabs> {
+  List<Categories> _imageList = List();
+  List<String> _categoryLsit = List();
+  List<int> _selectedIndexList = List();
+  bool _selectionMode = false;
+  List<Categories> cList = List();
+  int _tabIndex = 0;
+  TabController controller;
+
+  _MyHomePageState({this.controller});
+
+  var categoryImageAPI = "http://convergepro.xyz/meetupapi/cat_img/";
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _buttons = List();
+
+    if (_selectionMode) {
+      _buttons.add(IconButton(
+          icon: new Text("Next"),
+          onPressed: () {
+//            Navigator.push(
+//              context,
+//              MaterialPageRoute(
+//                  builder: (context) => InviteListScreen(usernameController)));
+
+            controller.animateTo(0);
+
+          }));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Categories"),
+        actions: _buttons,
+      ),
+      body: _createBody(),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    cList = new CategoryClass().getCategoryList();
+
+  }
+
+  void _changeSelection({bool enable, int index}) {
+    _selectionMode = enable;
+    _selectedIndexList.add(index);
+    if (index == -1) {
+      _selectedIndexList.clear();
+    }
+  }
+
+  Widget _createBody() {
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 2,
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+      primary: false,
+      itemCount: cList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return getGridTile(index);
+      },
+      staggeredTileBuilder: (int index) => StaggeredTile.count(1, 1),
+      padding: const EdgeInsets.all(4.0),
+    );
+  }
+
+  GridTile getGridTile(int index) {
+    if (_selectionMode) {
+      return GridTile(
+        header: GridTileBar(
+          leading: Icon(
+            _selectedIndexList.contains(index)
+                ? Icons.check_circle_outline
+                : Icons.radio_button_unchecked,
+            color: _selectedIndexList.contains(index)
+                ? Colors.green
+                : Colors.black,
           ),
         ),
+        child: GestureDetector( child: Container(
+
+            decoration: BoxDecoration(color: Colors.pink[50],shape: BoxShape.rectangle),
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 120,
+                  height: 120,
+                  alignment: Alignment.bottomCenter,
+                  padding: EdgeInsets.all(50),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(categoryImageAPI + cList[index].imgUrl),
+                          fit: BoxFit.cover)),
+
+                ),
+                Text(
+                  cList[index].txtCategoryName,
+                  textAlign: TextAlign.center,style: TextStyle(fontSize:15,),
+                ),
+              ],
+            )
+        ),
+          onLongPress: () {
+            setState(() {
+              _changeSelection(enable: false, index: -1);
+            });
+          },
+          onTap: () {
+            setState(() {
+              if (_selectedIndexList.contains(index)) {
+                _selectedIndexList.remove(index);
+              } else {
+                _selectedIndexList.add(index);
+              }
+            });
+          },
+
+      ));
+    } else {
+      return GridTile( child: GestureDetector(child: Container(
+
+          decoration: BoxDecoration(color: Colors.pink[50],shape: BoxShape.rectangle),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 120,
+                height: 120,
+                alignment: Alignment.bottomCenter,
+                padding: EdgeInsets.all(50),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(categoryImageAPI + cList[index].imgUrl),
+                        fit: BoxFit.cover)),
+
+              ),
+              Text(
+                cList[index].txtCategoryName,
+                textAlign: TextAlign.center,style: TextStyle(fontSize:15,),
+              ),
+            ],
+          )
       ),
-    );
+        onLongPress:() {
+        setState(() {
+          _changeSelection(enable: true, index: index);
+        });
+        },
+      ),
+      );
+
+    }
   }
 }
